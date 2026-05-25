b900c335c772e527a2212d7c9f42877e93cf65d4
Cauã <caua11mendes@gmail.com>
2026-05-25 13:32:30 -0300

    Atividades de funções

---
+++ ap3_funcoes/ap1.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+void main() {
+  final lists = Lista();
+  Soma(lists[0], lists[1]);
+}
+
+List<List<int>> Lista() {
+  final random = Random();
+
+  final lista1 = List.generate(5,  (_) => random.nextInt(100));
+
+  print("Lista: ${lista1.join(", ")}");
+
+  final lista2 = List.generate(5,  (_) => random.nextInt(100));
+
+  print("Lista: ${lista2.join(", ")}");
+
+  return [lista1, lista2];
+}
+
+void Soma(List<int> lista1, List<int> lista2) {
+
+  if (lista1.isEmpty || lista2.isEmpty) {
+    print("Lista vazia");
+    return;
+  }
+
+  if (lista1.length != lista2.length) {
+    print([]);
+    return;
+  }
+
+  final soma = <int>[];
+
+  for(int i = 0; i < lista1.length; i++) {
+    print("${lista1[i]} + ${lista2[i]}");
+  }
+
+  for(int i = 0; i < lista1.length; i++)
+  soma.add(lista1[i] + lista2[i]);
+    print("Lista: ${soma.join(", ")}");
+}
---
+++ ap3_funcoes/ap2.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+void main () {
+  funcA(funcB, funcC);
+  
+}
+
+void funcA(void Function(int, int) operacao1, void Function(int, int) operacao2) {
+  final random = Random();
+
+  for (int i = 0; i < 1; i++) {
+    int a = random.nextInt(100);
+    int b = random.nextInt(100);
+    operacao1(a, b);
+  }
+
+  for (int i = 0; i < 1; i++) { 
+    int c = random.nextInt(100);
+    int d = random.nextInt(100);
+    operacao2(c, d);
+  }
+}
+void funcB(int a, int b) {
+  print("$a + $b = ${a + b}");
+}
+
+void funcC(int c, int d) {
+  print("$c * $d = ${c * d}");
+}
