958aece8489240337bd617c2d7a14ae4cf114e4c
Cauã <caua11mendes@gmail.com>
2026-05-25 14:46:39 -0300

    Atividades de funções

---
+++ ap3_funcoes/ap3.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+void main () {
+  for (int raio in raio()) {
+    print(
+      "Raio: $raio, "
+      "Área: ${area(raio).toStringAsFixed(2)}, "
+      "Perímetro: ${perimetro(raio).toStringAsFixed(2)}"
+    );
+  }
+}
+
+List<int> raio() {
+  final random = Random();
+
+  final raio = List.generate(10,  (_) => random.nextInt(100));
+
+  return raio;
+}
+
+double area(int raio){
+  double area = pi * pow(raio, 2);
+  return area;
+} 
+
+double perimetro(int raio) {
+  double perimetro = 2 * pi * raio;
+  return perimetro;
+}
---
+++ ap3_funcoes/ap4.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+void main() {
+  for (int numero in numeros()) {
+    print(
+      "Número: $numero, "
+      "Decimal: ${decimal(numero)}, "
+      "Binário: ${binario(numero)}, "
+      "Octal: ${octal(numero)}, "
+      "Hexadecimal: ${hexadecimal(numero)}"
+    );
+  }
+}
+
+List<int> numeros() {
+  final random = Random();
+  final numeros = List.generate(15,  (_) => random.nextInt(5000));
+  print("Números: ${numeros.join(", ")}");  
+
+  return numeros;
+}
+
+int decimal(int numero) {
+  return numero;
+}
+
+String binario(int numero) {
+  return numero.toRadixString(2);
+}
+
+String octal(int numero) {
+  return numero.toRadixString(8);
+}
+
+String hexadecimal(int numero) {
+  return numero.toRadixString(16);
+}
